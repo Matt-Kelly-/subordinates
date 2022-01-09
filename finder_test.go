577633affdbb25e28a40c96f4d3c3814dc436797
Matt-Kelly- <Matt-Kelly-@users.noreply.github.com>
2022-01-09 03:15:25 +0000

    Add sample tests

---
+++ finder_test.go
@@ -0,0 +1,113 @@
+package subordinates_test
+
+import (
+	"github.com/Matt-Kelly-/subordinates"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetSubordinatesWithSampleData(t *testing.T) {
+	roles := []subordinates.Role{
+		{
+			Id:     1,
+			Name:   "System Administrator",
+			Parent: 0,
+		},
+		{
+			Id:     2,
+			Name:   "Location Manager",
+			Parent: 1,
+		},
+		{
+			Id:     3,
+			Name:   "Supervisor",
+			Parent: 2,
+		},
+		{
+			Id:     4,
+			Name:   "Employee",
+			Parent: 3,
+		},
+		{
+			Id:     5,
+			Name:   "Trainer",
+			Parent: 3,
+		},
+	}
+	users := []subordinates.User{
+		{
+			Id:   1,
+			Name: "Adam Admin",
+			Role: 1,
+		},
+		{
+			Id:   2,
+			Name: "Emily Employee",
+			Role: 4,
+		},
+		{
+			Id:   3,
+			Name: "Sam Supervisor",
+			Role: 3,
+		},
+		{
+			Id:   4,
+			Name: "Mary Manager",
+			Role: 2,
+		},
+		{
+			Id:   5,
+			Name: "Steve Trainer",
+			Role: 5,
+		},
+	}
+
+	finder := subordinates.NewFinder()
+	finder.SetRoles(roles)
+	finder.SetUsers(users)
+
+	t.Run("sample case 1", func(t *testing.T) {
+		results, err := finder.GetSubordinates(3)
+		require.Nil(t, err)
+		require.ElementsMatch(t, results, []subordinates.User{
+			{
+				Id:   2,
+				Name: "Emily Employee",
+				Role: 4,
+			},
+			{
+
+				Id:   5,
+				Name: "Steve Trainer",
+				Role: 5,
+			},
+		})
+	})
+
+	t.Run("sample case 2", func(t *testing.T) {
+		results, err := finder.GetSubordinates(1)
+		require.Nil(t, err)
+		require.ElementsMatch(t, results, []subordinates.User{
+			{
+				Id:   2,
+				Name: "Emily Employee",
+				Role: 4,
+			},
+			{
+				Id:   3,
+				Name: "Sam Supervisor",
+				Role: 3,
+			},
+			{
+				Id:   4,
+				Name: "Mary Manager",
+				Role: 2,
+			},
+			{
+				Id:   5,
+				Name: "Steve Trainer",
+				Role: 5,
+			},
+		})
+	})
+}
